cc52d3444195222d211e1762f11c5f959e922449
Simon Maher <simon.maher@integrify.io>
2019-07-15 19:49:32 +0300

    Created and sent initial api request

--- forecast.js
+++ forecast.js
@@ -1,1 +1,16 @@
 const key = 'f6qjckL2aLUMct6sAfgAz1wQI89CMCRD'
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+    // Returns data at index 0 of the array
+    return data[0];
+
+}
+getCity('helsinki')
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
